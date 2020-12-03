f9a4f501307dea73da63205de98475fe9b22ef2f
SenorYaoGuai <58443692+SenorYaoGuai@users.noreply.github.com>
2020-12-03 11:16:23 -0600

    Add files via upload

---
+++ index.hyperesources/triviaInfo.js
@@ -0,0 +1,44 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"Destiny TRIVIA";
+	window.questions = [		"WHICH WORM GOD GAVE ORYX THE POWER TO TAKE", 
+								"WHO WAS THE FIRST GOD THE GUARDIAN KILLED",
+								"WHO IS THE MAN THE MYTH THE LEGEND", 
+								"WHICH WARLOCK WAS KILLED BY THIER FATHER", 
+								"WHO FAKED THEIR DEATH AT TWILIGHT GAP" ];
+	window.correctAnswer = [ 	"D", "E", "B", "A", "C" ]; 
+	window.answerA = [ 			"YUL", 
+								"SEPKIS PRIME", 
+								"MASTER CHEIF", 
+								"FELWINTER", 
+								"SAINT-14" ];
+	window.answerB = [ 			"EIR", 
+								"THE UNDYING MIND", 
+								"RANDLE THE VANDLE", 
+								"ASHER MIR",
+								"OSIRIS" ];
+	window.answerC = [ 			"XOL",
+								"PHOGOTH",
+								"S.A.B.E.R.-2", 
+								"IKORA REY", 
+								"ANA BREY" ];
+	window.answerD = [ 			"AKKA",  
+								"ATHEON",
+								"LORD SHAXX", 
+								"GALLIDA TUYET",
+								"LORD SHAXX" ];
+	window.answerE = [ 			"UR", 
+								"CROTA SON OF ORYX", 
+								"SHIN MALPHUR", 
+								"ERIANA-3", 
+								"LORD SALADIN" ];
+	window.response = [ 		"ORYX GAIND THE POWER TO TAKE AFTER HE TOOK IT FROM AKKA", 
+								"CROTA WAS THE FIRST OF MANY GODS TO BE SLAN", 
+								"ITS NONE OTHER THAN RANDLE THE VANDLE, THE FIRST RAID BOSS", 
+								"FELWINTER WAS TRICKED BY HIS FATHER RASPUTIN AND KILLED SOON AFTER", 
+								"SHE DID IT TO WORK IN SECERT ON RASPUTIN" ];
+							
+}
+						
